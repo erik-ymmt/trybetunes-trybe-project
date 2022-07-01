8ae74ed554083dc3ae0e7e893d35bfa9e91a32b5
Erik Yamamoto <erik.ymmt@gmail.com>
2022-07-01 15:26:56 -0300

    add Routes - Requirement 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } exact />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/" component={ Login } exact />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+
+      </>
+    );
   }
 }
 
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        404
+      </div>
+    );
+  }
+}
+
+export default NotFound;
