d4f43e6a66c6a0c75a303532d8688d6cc749b9c3
Erik Yamamoto <erik.ymmt@gmail.com>
2022-07-01 19:58:22 -0300

    add Redirect after Login - Requirement 2

--- src/App.js
+++ src/App.js
@@ -7,46 +7,10 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
-import { createUser } from './services/userAPI';
+
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isBtnDisabled: true,
-      login: '',
-      isLoading: false,
-      loggedIn: false,
-    };
-  }
-
-  handleInput = ({ target }) => {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
-    }, this.loginValidation);
-  };
-
-  loginValidation = () => {
-    const { login } = this.state;
-    const minLoginLength = 3;
-    if (login.length >= minLoginLength) {
-      this.setState({ isBtnDisabled: false });
-    } else {
-      this.setState({ isBtnDisabled: true });
-    }
-  };
-
-  handleLogin = async () => {
-    const { login } = this.state;
-    const createUserArgument = { name: login };
-    this.setState({ isLoading: true });
-    await createUser(createUserArgument);
-    this.setState({ loggedIn: true });
-  }
-
   render() {
-    const { isBtnDisabled, login, isLoading, loggedIn } = this.state;
     return (
       <>
         <p>TrybeTunes</p>
@@ -61,12 +25,6 @@ class App extends React.Component {
             path="/"
             render={ (props) => (
               <Login
-                name="login"
-                isBtnDisabled={ isBtnDisabled }
-                handleClick={ this.handleLogin }
-                handleChange={ this.handleInput }
-                loginName={ login }
-                isLoading={ isLoading }
                 { ...props }
               />
             ) }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,47 +1,78 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
-  render() {
-    const { isBtnDisabled, handleClick, handleChange, loginName, isLoading } = this.props;
-    return isLoading
-      ? <Loading />
-      : (
-        <div data-testid="page-login">
-          <h2>Login</h2>
-          <form>
-            <label htmlFor="login">
-              <input
-                type="text"
-                name="login"
-                id="login"
-                data-testid="login-name-input"
-                onChange={ handleChange }
-                value={ loginName }
-              />
-            </label>
-            <button
-              type="button"
-              data-testid="login-submit-button"
-              disabled={ isBtnDisabled }
-              onClick={ handleClick }
-            >
-              Entrar
-            </button>
-          </form>
+  constructor() {
+    super();
+    this.state = {
+      isBtnDisabled: true,
+      loginName: '',
+      isLoading: false,
+      loggedIn: false,
+    };
+  }
 
-        </div>
-      );
+  handleInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.loginValidation);
+  };
+
+  loginValidation = () => {
+    const { loginName } = this.state;
+    const minLoginLength = 3;
+    if (loginName.length >= minLoginLength) {
+      this.setState({ isBtnDisabled: false });
+    } else {
+      this.setState({ isBtnDisabled: true });
+    }
+  };
+
+  handleLogin = async () => {
+    const { loginName } = this.state;
+    const createUserArgument = { name: loginName };
+    this.setState({ isLoading: true });
+    await createUser(createUserArgument);
+    this.setState({ isLoading: false });
+    this.setState({ loggedIn: true });
+  }
+
+  render() {
+    const { isBtnDisabled, isLoading, loggedIn, loginName } = this.state;
+    if (isLoading) {
+      return <Loading />;
+    } if (loggedIn) {
+      return <Redirect to="/search" />;
+    }
+    return (
+      <div data-testid="page-login">
+        <h2>Login</h2>
+        <form>
+          <label htmlFor="login">
+            <input
+              type="text"
+              name="loginName"
+              id="login"
+              data-testid="login-name-input"
+              onChange={ this.handleInput }
+              value={ loginName }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ isBtnDisabled }
+            onClick={ this.handleLogin }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
-
-Login.propTypes = {
-  isBtnDisabled: PropTypes.bool.isRequired,
-  handleClick: PropTypes.func.isRequired,
-  handleChange: PropTypes.func.isRequired,
-  loginName: PropTypes.string.isRequired,
-  isLoading: PropTypes.bool.isRequired,
-};
 
 export default Login;
