6fb772e6ab6386eff68db6919fdc6e787e8eabbf
Erik Yamamoto <erik.ymmt@gmail.com>
2022-07-02 18:08:49 -0300

    starting requirement 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Header from './Header';
 
 class Search extends React.Component {
@@ -7,6 +8,9 @@ class Search extends React.Component {
     this.state = {
       searchInput: '',
       isBtnDisabled: true,
+      isSearchLoading: false,
+      isSearchDone: true,
+      searchInputHistory: '',
     };
   }
 
@@ -27,33 +31,56 @@ class Search extends React.Component {
     }
   };
 
+  handleSearch = () => {
+    const { searchInput } = this.state;
+    this.setState({ searchInputHistory: searchInput });
+    this.setState({ searchInput: '' });
+    // chamar um state resultado da fetchAlbums();
+  }
+
+  fetchAlbums = async () => {
+    const { searchInput } = this.state;
+    this.setState({ isSearchLoading: true });
+    await searchAlbumsAPI(searchInput);
+  }
+
   render() {
-    const { searchInput, isBtnDisabled } = this.state;
+    const {
+      searchInput, isBtnDisabled, isSearchLoading, searchInputHistory, isSearchDone,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         Search
-        <form>
-          <label htmlFor="search-input">
-            <input
-              type="text"
-              name="searchInput"
-              id="search-input"
-              data-testid="search-artist-input"
-              onChange={ this.handleInput }
-              value={ searchInput }
-            />
-          </label>
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ isBtnDisabled }
-            onClick={ this.handleLogin }
-          >
-            Procurar
-          </button>
-
-        </form>
+        { isSearchLoading ? <p>Carregando...</p>
+          : (
+            <form>
+              <label htmlFor="search-input">
+                <input
+                  type="text"
+                  name="searchInput"
+                  id="search-input"
+                  data-testid="search-artist-input"
+                  onChange={ this.handleInput }
+                  value={ searchInput }
+                />
+              </label>
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ isBtnDisabled }
+                onClick={ this.handleSearch }
+              >
+                Procurar
+              </button>
+            </form>)}
+        { isSearchDone ? (
+          <h2>
+            Resultado de álbuns de:
+            {' '}
+            { searchInputHistory }
+          </h2>)
+          : <p>Else</p>}
       </div>
     );
   }
