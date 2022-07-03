21c4444738834bad2e9e906698498953ca6c3f4d
Erik Yamamoto <erik.ymmt@gmail.com>
2022-07-03 18:08:33 -0300

    add conditional for search - Requirement 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Header from './Header';
 
@@ -9,8 +10,9 @@ class Search extends React.Component {
       searchInput: '',
       isBtnDisabled: true,
       isSearchLoading: false,
-      isSearchDone: true,
+      isSearchDone: false,
       searchInputHistory: '',
+      albums: [],
     };
   }
 
@@ -34,20 +36,35 @@ class Search extends React.Component {
   handleSearch = () => {
     const { searchInput } = this.state;
     this.setState({ searchInputHistory: searchInput });
-    this.setState({ searchInput: '' });
+    this.setState({ searchInput: '' }, this.fetchAlbums);
     // chamar um state resultado da fetchAlbums();
   }
 
   fetchAlbums = async () => {
-    const { searchInput } = this.state;
+    const { searchInputHistory } = this.state;
     this.setState({ isSearchLoading: true });
-    await searchAlbumsAPI(searchInput);
+    const responseAlbums = await searchAlbumsAPI(searchInputHistory);
+    this.setState({ isSearchLoading: false, isSearchDone: true, albums: responseAlbums });
+  }
+
+  showFoundAlbums = () => {
+    const { albums } = this.state;
+    if (albums.length === 0) return <p>Nenhum álbum foi encontrado</p>;
+    return albums.map((album) => (
+      <Link
+        to={ `album/${album.collectionId}` }
+        key={ album.collectionId }
+        data-testid={ `link-to-album-${album.collectionId}` }
+      >
+        { album.collectionName }
+      </Link>
+    ));
   }
 
   render() {
     const {
-      searchInput, isBtnDisabled, isSearchLoading, searchInputHistory, isSearchDone,
-    } = this.state;
+      searchInput, isBtnDisabled, isSearchLoading,
+      searchInputHistory, isSearchDone } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -75,11 +92,15 @@ class Search extends React.Component {
               </button>
             </form>)}
         { isSearchDone ? (
-          <h2>
-            Resultado de álbuns de:
-            {' '}
-            { searchInputHistory }
-          </h2>)
+          <div>
+            <h2>
+              Resultado de álbuns de:
+              {' '}
+              { searchInputHistory }
+            </h2>
+            {this.showFoundAlbums()}
+          </div>
+        )
           : <p>Else</p>}
       </div>
     );
